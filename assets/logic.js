97d45dfb5b730f4bcc28ae82296247771e6b2723
Tsarina <tsarina.islam@gmail.com>
2019-07-03 15:08:59 -0400

    just some more functional shtuff

--- assets/logic.js
+++ assets/logic.js
@@ -1,19 +1,35 @@
-var searchArray = ["Ice cream", "cookies", "brownies"]
+$(document).ready(function () {
 
 
 
-function populateButtons(searchArray, classToAdd, areaToAddTo){
-    $(areaToAddTo).empty();
-    for (let i = 0; i < searchArray.length; i++) {
-        var searchedGif = $("<button");
-        searchedGif.addClass(classToAdd);
-        searchedGif.attr("data-type", searchArray[i]);
-        searchedGif.text(searchArray[i]);
-        $(areaToAddTo).append(searchedGif);
-        
+    var dessertsArray = ["Ice cream", "cookies", "brownies"]
+
+    // This makes buttons and adds gifs
+    function populateButtons(dessertsArray, classToAdd, areaToAddTo) {
+        $(areaToAddTo).empty();
+
+        for (let i = 0; i < dessertsArray.length; i++) {
+            var searchedGif = $("<button");
+            searchedGif.addClass(classToAdd);
+            searchedGif.attr("data-type", dessertsArray[i]);
+            searchedGif.text(dessertsArray[i]);
+            $(areaToAddTo).append(searchedGif);
+
+        }
     }
-}
 
-$(function() {
+    $(document).on("click", ".searchButton", function () {
+        $("#desserts").empty();
+        $(".searchButton").removeClass("active");
+        $(this).addClass("active");
+
+        var type = $(this).data("type");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q" + type + "&api_key=fLSK8kQLL6eVTpieZ7yy7EQ3fJa2HkPs" + "&limit=5";
+        $.ajax()
+    })
+
+    $(function () {
+
+    })
 
 })
