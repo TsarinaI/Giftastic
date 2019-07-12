83b1b3e4ba31fa4b4468088c8964a5cd5001215b
Tsarina <tsarina.islam@gmail.com>
2019-07-12 13:45:02 -0400

    lots of JS, hardly any of it working properly....

--- assets/logic.js
+++ assets/logic.js
@@ -9,7 +9,7 @@ $(document).ready(function () {
         $(areaToAddTo).empty();
 
         for (let i = 0; i < dessertsArray.length; i++) {
-            var searchedGif = $("<button");
+            var searchedGif = $("<button>");
             searchedGif.addClass(classToAdd);
             searchedGif.attr("data-type", dessertsArray[i]);
             searchedGif.text(dessertsArray[i]);
@@ -23,13 +23,68 @@ $(document).ready(function () {
         $(".searchButton").removeClass("active");
         $(this).addClass("active");
 
-        var type = $(this).data("type");
+        var type = $(this).attr("data-type");
         var queryURL = "https://api.giphy.com/v1/gifs/search?q" + type + "&api_key=fLSK8kQLL6eVTpieZ7yy7EQ3fJa2HkPs" + "&limit=5";
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+            .then(function (response) {
+                var results = response.data;
+
+                for (var i = 0; i < results.length; i++) {
+                    var dessertsDiv = $("<div class=\"dessert-item\">");
+
+                    var rating = results[i].rating;
+
+                    var p = $("<p>").text("Rating: " + rating);
+
+                    var animated = results[i].images.fixed_height.url;
+                    var still = results[i].images.fixed_height_still.url;
+
+                    var dessertsImg = $("<img>");
+                    dessertsImg.attr("src", still);
+                    dessertsImg.attr("data-still", still);
+                    dessertsImg.attr("data-animate", animated);
+                    dessertsImg.attr("data-state", "still");
+                    dessertsImg.addClass("desserts-image");
+
+                    dessertsDiv.append(p);
+                    dessertsDiv.append(dessertsImg);
+
+                    $("#desserts").append(dessertsDiv);
+                }
+            });
+
         $.ajax()
     })
 
-    $(function () {
+    populateButtons(dessertsArray, ".searchButton", "#desserts");
 
-    })
+    $(document).on("click", ".desserts-image", function () {
+
+        var state = $(this).attr("data-state");
+
+        if (state === "still") {
+            $(this).attr("src", $(this).attr("data-animate"));
+            $(this).attr("data-state", "animate");
+        }
+        else {
+            $(this).attr("src", $(this).attr("data-still"));
+            $(this).attr("data-state", "still");
+        }
+    });
+
+    $("#add-dessert").on("click", function (event) {
+        event.preventDefault();
+        var newDessert = $("input").eq(0).val();
+
+        if (newDessert.length > 2) {
+            desserts.push(newDessert);
+        }
+
+        populateButtons(dessertsArray, ".searchButton", "#desserts");
+    });
 
 })
